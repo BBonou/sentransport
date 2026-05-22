cd07a76bdff8dec7c1f077a117e60f4c86ff92c2
BBonou <bidossessibenarielbonou@esp.sn>
2026-05-22 09:40:38 +0000

    Lab 4 Exercises : Addition of /arrets, /stats & /lignes/recherche?q=<name> endpoints

--- api/app.py
+++ api/app.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -14,7 +14,7 @@ with open("lignes_ddd.json", "r") as f:
 def acceuil():
     return jsonify({
         "message": "Bienvenue sur l'API SenTransport !",
-        "endpoints": ["/lignes", "/lignes/<id>"]
+        "endpoints": ["/lignes", "/lignes/<id>", "/arrets", "/stats", "/lignes/recherche?q=<nom>"]
     })
 
 @app.route("/lignes")
@@ -30,6 +30,48 @@ def get_ligne(ligne_id):
     if ligne is None:
         return jsonify({"erreur": "Ligne non trouvee"}), 404
     return jsonify(ligne)
+
+# ========== Exercises additions ==========
+
+# Exercise 1
+@app.route("/arrets")
+def get_arrets():
+    allStops = set()
+
+    for ligne in lignes:
+        allStops.update(ligne["listeArrets"])
+
+    return jsonify(list(allStops))
+
+# Exercise 2
+@app.route("/stats")
+def get_stats():
+    numLines = len(lignes)
+    totalStops = sum(ligne["arrets"] for ligne in lignes)
+
+    mostStopsLine = max(
+        lignes,
+        key=lambda ligne : ligne["arrets"]
+    )
+
+    return jsonify({
+        "nombre_total_lignes": numLines,
+        "nombre_total_arretts": totalStops,
+        "ligne_avec_plus_arrets": mostStopsLine["numero"],
+    })
+
+# Exercise 3
+@app.route("/lignes/recherche")
+def rechercher_ligne():
+    q = request.args.get("q", "").lower()
+
+    results = [
+        ligne for ligne in lignes
+        if q in ligne["depart"].lower()
+        or q in ligne["arrivee"].lower()
+    ]
+
+    return jsonify(results)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
